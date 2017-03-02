Fixed the Gradient Check function and completed the Neural Network portion of the assignment

assignment1/q1_softmax.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-def softmax(x):
+def softmax(X):
     """
     Compute the softmax function for each row of the input x.
 
@@ -20,18 +20,12 @@ def softmax(x):
     written assignment!
     """
 
-    dim = len(x.shape)
+    if X.ndim > 1:
+        diff = np.exp(X - X.max(axis=1)[:, np.newaxis])
+        return diff/np.sum(diff, axis = 1)[:, np.newaxis]
 
-    #diff = np.array([])
-
-    if dim > 1:
-        diff = x - np.amax(x, axis = 1, keepdims = True)
-
-        return np.exp(diff)/np.sum(np.exp(diff), axis = 1)
-
-    else:
-        diff = x - np.max(x)
-        return np.exp(diff)/np.sum(np.exp(diff))
+    diff = np.exp(X - np.max(X))
+    return diff/np.sum(diff)
 
 
 def test_softmax_basic():

assignment1/q2_gradcheck.py
@@ -8,12 +8,11 @@ def gradcheck_naive(f, x):
     - f should be a function that takes a single argument and outputs the cost and its gradients
     - x is the point (numpy array) to check the gradient at
     """
-
     rndstate = random.getstate()
     random.setstate(rndstate)
     fx, grad = f(x) # Evaluate function value at original point
     h = 1e-7
-
+    
     # Iterate over all indexes in x
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
@@ -22,13 +21,20 @@ def gradcheck_naive(f, x):
         ### make sure you call random.setstate(rndstate) before calling f(x) each time, this will make it
         ### possible to test cost functions with built in randomness later
         ### YOUR CODE HERE:
+        old_x = x[ix]
+        x[ix] = old_x + h
+        cost_new = f(x)[0]
         random.setstate(rndstate)
-        diff = f(it[0] + h)[0] - f(it[0])[0]
+        diff = cost_new - fx
         numgrad = (diff)/h
+        x[ix] = old_x
+
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
+
+        print ix
         if reldiff > 1e-5:
             print "Gradient check failed."
             print "First gradient error found at index %s" % str(ix)

assignment1/q2_neural.py
@@ -13,9 +13,11 @@ def forward_backward_prop(data, labels, params, dimensions):
     and backward propagation for the gradients for all parameters.
     """
 
+    num_train = data.shape[0]
     ### Unpack network parameters (do not modify)
     ofs = 0
     Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
 
     W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
     ofs += Dx * H
@@ -25,25 +27,39 @@ def forward_backward_prop(data, labels, params, dimensions):
     ofs += H * Dy
     b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
 
-    h = np.dot(W1.dot)
-    gradW1 =
-    graddb1
-    gradW2
-    gradb2
 
-    ### YOUR CODE HERE: forward propagation
-    raise NotImplementedError
-    ### END YOUR CODE
 
-    ### YOUR CODE HERE: backward propagation
-    raise NotImplementedError
-    ### END YOUR CODE
+
+    #Forward Feeding - calculating the loss
+    h = sigmoid(np.dot(data, W1) + b1)
+    
+    score = softmax(np.dot(h, W2) + b2)
+    loss = - np.sum(np.log(score[xrange(num_train), np.argmax(labels, axis = 1)]))
+
+    # Calculating gradient by taking care of gradient matrices in the hidden
+    # layers
+    score -= labels
+
+    grad_layer_2   = np.zeros_like(score)
+    grad_layer_1 = np.zeros_like(h)
+    gradW2    = np.zeros_like(W2)
+    gradW1    = np.zeros_like(W1)
+    gradb2    = np.zeros_like(b2)
+    gradb1    = np.zeros_like(b1)
+
+    gradW2 = np.dot(h.T, score)
+    gradb2 = np.sum(score, axis = 0)
+    grad_layer_2 = np.dot(score, W2.T)
+    grad_layer_1 = sigmoid_grad(h) * grad_layer_2
+
+    gradW1 = np.dot(data.T, grad_layer_1)
+    gradb1 = np.sum(grad_layer_1, axis = 0)
 
     ### Stack gradients (do not modify)
     grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
         gradW2.flatten(), gradb2.flatten()))
 
-    return cost, grad
+    return loss, grad
 
 def sanity_check():
     """
@@ -73,10 +89,6 @@ def your_sanity_checks():
     your additional tests be graded.
     """
     print "Running your sanity checks..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-
 if __name__ == "__main__":
     sanity_check()
     your_sanity_checks()